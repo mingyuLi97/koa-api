a36325aa3650240e98decf29e5fa55986b58c4f2
limy <mingyuli1997@gmail.com>
2023-05-26 16:28:31 +0800

    feat: 🎸 jwt 颁发 token

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,8 @@
+const jwt = require("jsonwebtoken");
+
 const { createUser, getUserInfo } = require("../service/user.service");
 const { USER_REGISTER_ERROR } = require("../constant/err.type");
+const { JWT_SECRET } = require("../config/config.default");
 class UserController {
   async register(ctx, next) {
     const { user_name, password } = ctx.request.body;
@@ -20,9 +23,24 @@ class UserController {
     }
   }
   async login(ctx, next) {
-    const { user_name, password } = ctx.request.body;
-
+    const { user_name } = ctx.request.body;
+    // 获取用户信息(在token的 payload 中，记录id，user_name，is admin
     ctx.body = `Hello "${user_name}"`;
+
+    try {
+      const { password, ...res } = await getUserInfo({ user_name });
+      ctx.body = {
+        code: 0,
+        message: "用户登录成功",
+        result: {
+          token: jwt.sign(res, JWT_SECRET, { expiresIn: "1d" }),
+        },
+      };
+    } catch (err) {
+      console.error("用户登录错误", err);
+      ctx.app.emit("error", USER_LOGIN_ERROR, ctx);
+      return;
+    }
   }
 }
 
