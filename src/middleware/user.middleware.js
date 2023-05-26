5a3ac20b8dc5f0a179b022b2cc152b33cce876f7
limy <mingyuli1997@gmail.com>
2023-05-26 15:40:41 +0800

    feat: 🎸 密码 加密存储

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,3 +1,4 @@
+const encrypt = require("bcryptjs");
 const { getUserInfo } = require("../service/user.service");
 const {
   USER_FORMATE_ERROR,
@@ -26,7 +27,18 @@ const verifyUser = async (ctx, next) => {
   await next();
 };
 
+const encryptPassword = async (ctx, next) => {
+  const { password } = ctx.request.body;
+
+  const salt = encrypt.genSaltSync(10);
+  const hash = encrypt.hashSync(password, salt);
+  ctx.request.body.password = hash;
+
+  await next();
+};
+
 module.exports = {
   userValidator,
   verifyUser,
+  encryptPassword,
 };
--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,11 +1,15 @@
 const Router = require("@koa/router");
 
-const { userValidator, verifyUser } = require("../middleware/user.middleware");
+const {
+  userValidator,
+  verifyUser,
+  encryptPassword,
+} = require("../middleware/user.middleware");
 const { register, login } = require("../controller/user.controller");
 const router = new Router({ prefix: "/users" });
 
 // 注册接口
-router.post("/register", userValidator, verifyUser, register);
+router.post("/register", userValidator, verifyUser, encryptPassword, register);
 router.post("/login", login);
 
 module.exports = router;
